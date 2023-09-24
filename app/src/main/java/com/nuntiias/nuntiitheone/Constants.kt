a21dffe266c56ed1d67ef83444aab9912484c073
JulianAngres <79283438+JulianAngres@users.noreply.github.com>
2023-09-24 11:22:58 +0200

    Update Constants.kt

--- app/src/main/java/com/nuntiias/nuntiitheone/Constants.kt
+++ app/src/main/java/com/nuntiias/nuntiitheone/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAmLnDFs8:APA91bEiouQuktsMPFUGWHWO1p7Yt-t6dnYYkv-vocJavSFITO_a2yJv1snxa5o4LxaFUDqmTrtcJka9ViEVkf5E0j4W_CoR7g7s25O0JhSNE2QGIwEIHFfnRmqiwUYIpd4GWSghiLi_"
+        const val SERVER_KEY = ""
         const val CONTENT_TYPE = "application/json"
     }
 }
